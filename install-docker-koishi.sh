eca230c7d32fe3515e6bb2e1137d9d378812c7fd
我怎么就不是一只猫呢? <26274059+dezhishen@users.noreply.github.com>
2024-03-28 16:29:53 +0800

    Create install-docker-koishi.sh

---
+++ install-docker-koishi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+base_data_dir=$1
+
+container_name=koishi
+port=5140
+image=koishijs/koishi:latest
+docker pull $image
+
+`dirname $0`/stop-container.sh ${container_name}
+docker run --name=${container_name} \
+        -d -it --restart=always \
+        -p ${port}:${port} \
+        -e PUID=`id -u` -e PGID=`id -g` \
+        -e TZ="Asia/Shanghai" \
+        -e LANG="zh_CN.UTF-8" \
+        -v ${base_data_dir}/${container_name}/:/koishi \
+${image}
